3ada02d87972ac692e30cd0c52a12fb059c3bc51
Vlad Zaharia <zahariavlad@outlook.com>
2021-05-03 21:32:56 +0200

    reMaj des noms de modules

---
+++ Modules/2_Buttons.py
@@ -0,0 +1,11 @@
+"""
+Author : Gabriel Lepinay | Vlad Zaharia
+Version : Python 3.7.3 - 32 bits
+IDE : Visual Studio Code
+Directory : /home/pi/Documents/Dev/B00mB0x/Modules/Module_2.py
+Description : 
+
+Module : 
+    Objective : 
+    Malus : 
+"""
