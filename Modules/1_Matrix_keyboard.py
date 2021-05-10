2a92b8a91940caef95fbec6648ebbda28f285c7e
Gxbyy <gaby.dev@outlook.fr>
2021-05-10 18:32:25 +0200

    Last checkpoint module 1

--- Modules/1_Matrix_keyboard.py
+++ Modules/1_Matrix_keyboard.py
@@ -14,9 +14,15 @@ import time
 import random
 from Passives.lcd import *
 from Passives.erreur import Error
+from Passives.buzzer import sonFinModule
 
+mcp.output(3,1)     # Allume la lumiere du lcd
+lcd.begin(16,2)     # Met le nombre du colonne et de ligne de l'écran
 Erreurs = Error()
 etape = 1
+listeTouches = []
+indexLettre = 0
+
 
 class keypad():
     # CONSTANTS   
@@ -92,118 +98,141 @@ class keypad():
 # Mot Court < 7 
 # 7 < Mot Normal < 11
 # Mot long > 11
-        
-def choixMot():        
-        mot = ""
-        motsCourt = ["FLUX", "LINUX", "HTML", "LOG", "WI FI", "LIEN", "INTEL", "OCTET", "VIRUS", "PYTHON"]
-        motsNormaux = ["LOGICIEL", "CONSOLE", "ORDINATEUR", "HACKEUR", "PIRATAGE", "INTERNET", "RESEAUX", "STOCKAGE", "ROUTEUR", "PARE FEU"]    
-        motsLongs = ["CRYPTOLOGIE", "APPLICATION", "DEVELOPPEUR","GABY ET VLAD"]
 
-        if etape == 1:
-            mot = motsCourt[random.randint(0, len(motsCourt)-1)]
-            lcd.setCursor(4,0)
-            lcd.message(mot)
-        elif etape == 2:
-            mot = motsNormaux[random.randint(0, len(motsCourt)-1)]
-            lcd.setCursor(4,0)
-            lcd.message(mot)
-        elif etape == 3:
-            mot = motsLongs[random.randint(0, len(motsCourt)-1)]
-            lcd.setCursor(4,0)
-            lcd.message(mot)
+def finEtape():
+    lcd.clear()
+    lcd.setCursor(0, 0)
+    for i in range(12):
+        lcd.scrollDisplayLeft()
+    lcd.message("Bien joué !\nMot suivant : ")
+    for i in range(28):      
+        lcd.scrollDisplayRight()
+        time.sleep(0.2)
+
+def choixMot():
+    """
+    Permet de choisir le mot a deviner pour cette enigme 
+    La difficulté est croissante
+    """           
+    mot = ""
+    motsCourt = ["FLUX", "LINUX", "HTML", "LOG", "WI_FI", "LIEN", "INTEL", "OCTET", "VIRUS", "PYTHON"]
+    motsNormaux = ["LOGICIEL", "CONSOLE", "ORDINATEUR", "HACKEUR", "PIRATAGE", "INTERNET", "RESEAUX", "STOCKAGE", "ROUTEUR", "PARE_FEU"]    
+    motsLongs = ["CRYPTOLOGIE", "APPLICATION", "DEVELOPPEUR","GABY_ET_VLAD"]
+
+    if etape == 1:
+        mot = motsCourt[random.randint(0, len(motsCourt)-1)]
+        lcd.setCursor(4,0)
+        lcd.message(mot)
+        return mot
+    elif etape == 2:
+        mot = motsNormaux[random.randint(0, len(motsCourt)-1)]
+        lcd.setCursor(4,0)
+        lcd.message(mot)
+        return mot
+    elif etape == 3:
+        mot = motsLongs[random.randint(0, len(motsCourt)-1)]
+        lcd.setCursor(4,0)
+        lcd.message(mot)
+        return mot
+
+def accept(mdp):
+    """
+    Code executer lorsque l'utilisateur rentre la combinaison de chiffre et appuis sur A
+    """
+    global indexLettre
+    global etape
+    print("Accepted")
+    verification = conversion()
+    if verification == mdp[indexLettre]:
+        print("Lettre validé")
+        lcd.setCursor(indexLettre, 1)
+        lcd.message(verification)
+        indexLettre += 1
+        listeTouches.clear()
+        if indexLettre >= len(mdp):
+            print("Fin du mot") # Mot réussi
+            etape += 1
+            finEtape()
+            module_1()
+    else:
+        Erreurs.__add__()
+        lcd.setCursor(indexLettre, 1)
+        lcd.message("/")
+        time.sleep(0.7)
+
+    
+    def delete():
+        pass #Pas bsoin finalement
+
+def clear():
+    """
+    Code executer lorsque l'utilisateur appuis sur D
+    Permet de reset les chiffres sur lequels il à appuyer
+    """
+    listeTouches.clear()
+    print("cleared")
 
 MultiTap = {"A":"accept()", "C":"cancel()", "D":"delete()", # A changer
-            "0":" ",
+            "0":"_",
             "8":"A", "88":"B", "888":"C", "1":"D", "11":"E", "6":"F", "66":"G", "666":"H", "3":"I", "33":"J", "7":"K", "77":"L", "#":"M", "##":"N", "###":"O", "9":"P", "99":"Q", "4":"R", "44":"S", "*":"T", "**":"U", "***":"V", "5":"W", "55":"X", "2":"Y", "22":"Z"}
 
-listeTouches = []
-listeLettres = []
-
-def hashage():
+def conversion():
     """
-    Transforme la liste des chiffres en chaine de caractère de chiffre
-    Correspondant donc au type du dictionnaire MultiTap
+    Changement des touches rentées par l'utilisateur en lettres
     """
-    lettre = ""
-    for i in range(len(listeTouches)):
-        lettre += listeTouches[i]
+    chiffres = ""
 
-    return lettre
+    if len(listeTouches) != 0:
 
-def conversion(touche, lettre):
-    """
-    Changement de la touche en lettres
-    """
-    index = len(listeLettres)
-
-
-    if len(listeTouches) > 1: # Il y a au moins 1 nombre ?
-        #Oui
-        if listeTouches[len(listeTouches) - 2] == touche: #Est ce que c'est le même ? (-2 Car la longueur d'une liste commence a 1 et la liste des éléments à 0 et on veut l'element d'avant)
-            #Oui
-            if listeTouches < 3:
-                #Oui
-                listeLettres.append(MultiTap.get(hashage()))
-                lcd.setCursor(index, 1)
-                lcd.message(listeLettres(index))
-
-
-            else:
-                #Non
-                listeTouches.clear()
-                listeTouches.append(touche[0])
-                lcd.setCursor(index, 1)
-                lcd.message(MultiTap.get(touche))
-
-        else:
-            #Non
-            listeLettres.append(MultiTap.get(listeTouches[len(listeTouches) - 2]))
-            lcd.setCursor(index, 1)
-            lcd.message(listeLettres(index))
-            listeTouches.clear()
-            listeTouches.append(touche[0])
+        if len(listeTouches) <= 3:
+        
+            for i in range(len(listeTouches)):
+                chiffres += listeTouches[i]    
+            return (MultiTap.get(chiffres))
     else:
-        #Non
-        lcd.setCursor(index, 1)
-        lcd.message(MultiTap.get(listeTouches[0]))
-
-
-
+        pass
 
 
 def module_1():
+    """
+    Code principal de l'enigme 
+    """
     print("Lancement du module 1 ...")    
     # Initialize the keypad class
     kp = keypad()
-    choixMot()
-    Erreurs = Error()
+    lcd.clear()
     Erreurs.__str__()
+    motDePasse = choixMot()
+    print(motDePasse)
     # Loop while waiting for a keypress
     while etape != 4:
         digit = None
         while digit == None:
             digit = kp.getKey()   
         # Print the result
-        listeTouches.append(str(digit))
-        conversion(listeTouches, listeLettres)
-        time.sleep(0.7)
+        if digit == "A":
+            accept(motDePasse)
+        elif digit == "B":
+            pass
+        elif digit == "C":
+            clear()
+        elif digit == "D":
+            delete()
+        else:
+            listeTouches.append(str(digit))
+        print(listeTouches)
+        time.sleep(0.5)
 
     else:
         print("Module reussi")
-
-    # Loop while waiting for a keypress
-   
 
 
 
 if __name__ == '__main__': 
     try:
-        mcp.output(3,1)     # turn on LCD backlight
-        lcd.begin(16,2)     # set number of LCD lines and columns
         module_1()
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the flowing code will be  executed.
-        GPIO.cleanup()                     # Release resource
-        lcd.clear()
-        lcd.noDisplay()
+    except KeyboardInterrupt:  # Quand 'Ctrl+C' est pressé, le code si dessous s'éxecute.                 
+        lcd.clear() # Clear l'écran
+        GPIO.cleanup() # Laisse les ressources
 
 
