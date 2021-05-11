8d4bf8c8dce02295664112f69a2497c315065c27
Gxbyy <gaby.dev@outlook.fr>
2021-05-11 21:10:56 +0200

    bigoPassword

--- Modules/1_bigoPassword.py
+++ Modules/1_bigoPassword.py
@@ -10,13 +10,14 @@ Module : Matrix keyboard
     Malus : - 10 seconds
 """
 import random
-from Hardware.display import *
-from Hardware.Software.erreur import Error
-from Hardware.buzzer import sonFinModule, sonFinEtape
 
 from Passives.buzzer import sonFinEtape, sonFinModule
 from Passives.erreur import Error
 from Passives.lcd import *
+
+from Hardware.buzzer import sonFinEtape, sonFinModule
+from Hardware.display import *
+from Hardware.Software.erreur import Error
 
 mcp.output(3,1)     # Allume la lumiere du display
 display.begin(16,2)     # Met le nombre du colonne et de ligne de l'écran
@@ -24,81 +25,6 @@ Erreurs = Error()
 etape = 1
 listeTouches = []
 indexLettre = 0
-
-
-class keypad():
-    # CONSTANTS   
-    KEYPAD = [
-    [1,2,3,"A"],
-    [4,5,6,"B"],
-    [7,8,9,"C"],
-    ["*",0,"#","D"]
-    ]
-     
-    # ROW = [11,12,13,15]        #C'est tes pins gabys
-    # COLUMN = [16,18,22,7]
-
-    ROW = [18,16,15,13]      #la c'est les miens
-    COLUMN = [12,10,8,7]
-
-    def __init__(self):
-        GPIO.setmode(GPIO.BOARD)
-
-    def getKey(self):
-         
-        # Set all columns as output low
-        for j in range(len(self.COLUMN)):
-            GPIO.setup(self.COLUMN[j], GPIO.OUT)
-            GPIO.output(self.COLUMN[j], GPIO.LOW)
-         
-        # Set all rows as input
-        for i in range(len(self.ROW)):
-            GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-         
-        # Scan rows for pushed key/button
-        # A valid key press should set "rowVal"  between 0 and 3.
-        rowVal = -1
-        for i in range(len(self.ROW)):
-            tmpRead = GPIO.input(self.ROW[i])
-            if tmpRead == 0:
-                rowVal = i
-                 
-        # if rowVal is not 0 thru 3 then no button was pressed and we can exit
-        if rowVal < 0 or rowVal > 3:
-            self.exit()
-            return
-         
-        # Convert columns to input
-        for j in range(len(self.COLUMN)):
-            GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-         
-        # Switch the i-th row found from scan to output
-        GPIO.setup(self.ROW[rowVal], GPIO.OUT)
-        GPIO.output(self.ROW[rowVal], GPIO.HIGH)
- 
-        # Scan columns for still-pushed key/button
-        # A valid key press should set "colVal"  between 0 and 2.
-        colVal = -1
-        for j in range(len(self.COLUMN)):
-            tmpRead = GPIO.input(self.COLUMN[j])
-            if tmpRead == 1:
-                colVal=j
-                 
-        # if colVal is not 0 thru 2 then no button was pressed and we can exit
-        if colVal < 0 or colVal > 3:
-            self.exit()
-            return
- 
-        # Return the value of the key pressed
-        self.exit()
-        return self.KEYPAD[rowVal][colVal]
-         
-    def exit(self):
-        # Reinitialize all rows and columns as input at exit
-        for i in range(len(self.ROW)):
-                GPIO.setup(self.ROW[i], GPIO.IN, pull_up_down=GPIO.PUD_UP) 
-        for j in range(len(self.COLUMN)):
-                GPIO.setup(self.COLUMN[j], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Mot Court < 7 
 # 7 < Mot Normal < 11
