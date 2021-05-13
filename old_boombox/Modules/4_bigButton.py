a36fd1ffff81a63676c19a5596e488a1450b4a00
vladzhra <zahariavlad@outlook.com>
2021-05-13 22:44:34 +0200

    petite correction

--- old_boombox/Modules/4_bigButton.py
+++ old_boombox/Modules/4_bigButton.py
@@ -15,7 +15,7 @@ import time
 import random
 
 pins = [11, 12, 13]     # define the pins for R:11,G:12,B:13 
-buttonPin = 19  # define buttonPin
+buttonPin = 32    # define buttonPin
 
 def setup():
     global pwmRed,pwmGreen,pwmBlue  
@@ -67,10 +67,6 @@ def module_4():
     print("Lancement du module 4 ...")
     while True:
         if GPIO.input(buttonPin)==GPIO.LOW: # if button is pressed
-            if buttonPin == 13:
-                return Bleu
-            
-            print("Button is pressed")
             blanc()
             time.sleep(0.1)
             rouge()
@@ -86,13 +82,14 @@ def module_4():
             rose()
             time.sleep(0.1)
             violet()
-            time.sleep(0.1)             
+            time.sleep(0.1)            
 
         else : # if button is relessed
             noir()
 
 
 
+ 
 
 
 
